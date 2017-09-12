14cda4c4e5be57cad94a8b54e93d1d6525f01571
Alexander Suvorov <a.suvorof@gmail.com>
2017-09-13 00:42:00 +0400

    Вариант первой страницы

--- js_dev/main.js
+++ js_dev/main.js
@@ -14,10 +14,13 @@ $(document).ready(function() {
         redvus_logo = $('#redvus_logo');
         redvus_shutter_left = $('.shutter-left');
         redvus_shutter_right = $('.shutter-right');
-        redvus_menu = $('#redvus_menu');
-        redvus_menu_v = $('#redvus_menu_v');
-        redvus_menu_left = $('#redvus_menu_left');
-        redvus_menu_right = $('#redvus_menu_right');
+        redvus_menu = $('#menu');
+        redvus_menu_v = $('#menu_v');
+        redvus_menu_left = $('#menu_left_line');
+        redvus_menu_right = $('#menu_right_line');
+        redvus_menu_up = $('#menu_up_line');
+        redvus_menu_down = $('#menu_down_line');
+        redvus_menu_cross = $('#menu_cross');
 
     var slogan_R = $('#slogan_R'),
         slogan_real = $('#slogan_real'),
@@ -36,7 +39,10 @@ $(document).ready(function() {
 
     var redvus_logo_tl = new TimelineMax();
 
-    redvus_logo_tl.from(redvus_logo_v_pencil, 1, {x: -500, ease:Power1.easeOut})
+    redvus_logo_tl
+        .set(redvus_menu_cross, {scale: 0, transformOrigin: '50% 50%', autoAlpha: 0})
+        .to(redvus_logo, 0.5, {autoAlpha: 1, ease:Power0.easeIn})
+        .from(redvus_logo_v_pencil, 1, {x: -500, ease:Power1.easeOut})
         .from(redvus_logo_line_v, 1, {x: -500, ease:Power1.easeOut}, '-=1')
         .from(redvus_logo_d, 0.6, {x: -300, ease:Bounce.easeOut}, '-=0.2')
         .from(redvus_logo_r, 0.5, {x: -200, ease:Back.easeOut})
@@ -48,10 +54,12 @@ $(document).ready(function() {
         .staggerFrom([redvus_logo_studio_s,redvus_logo_studio_t,redvus_logo_studio_u,redvus_logo_studio_d,redvus_logo_studio_i,redvus_logo_studio_o], 0.3, {x: 300, ease:Back.easeOut}, 0.2)
         .to(redvus_logo, 1, {scale: '0.5', ease:Power3.easeOut})
         .to(redvus_shutter_left, 2, {width: '25%', ease:Back.easeOut}, '-=1')
-        .to(redvus_shutter_right, 2, {width: '25%', ease:Back.easeOut}, '-=1')
+        .to(redvus_shutter_right, 2, {width: '25%', ease:Back.easeOut}, '-=2')
+        .to(redvus_menu, 0.5, {autoAlpha: 1, ease:Power0.easeIn}, '-=1')
         .from(redvus_menu_left, 0.8, {y: 100, ease:Power1.easeOut}, '-=1')
         .from(redvus_menu_right, 0.8, {y: 100, ease:Power1.easeOut}, '-=0.5')
-        .from(redvus_menu_v, 0.8, {y: -100, ease:Back.easeOut}, '-=0.3');
+        .from(redvus_menu_v, 0.8, {y: -100, ease:Back.easeOut}, '-=0.3')
+        ;
 
     var redvus_slogan_tl = new TimelineMax();
 
@@ -81,10 +89,15 @@ $(document).ready(function() {
     ============================*/
 
     var redvus_menu_open = new TimelineMax({paused:true, reversed:true});
-    redvus_menu_open.to(redvus_shutter_right, 0.4, {width: '30%', ease:Back.easeOut})
-        .to(redvus_shutter_left, 0.4, {width: '20%', ease:Back.easeOut}, '-=0.4')
-        .to(redvus_menu, 1, {x: '-405%', rotation: '405', ease:Back.easeOut})
-        .to(redvus_menu_v, 0.8, {y: -100, autoAlpha: 0, ease:Back.easeOut}, '-=1');
+    redvus_menu_open
+        .to(redvus_shutter_right, 0.8, {width: '50%', ease:Back.easeInOut})
+        .to(redvus_shutter_left, 0.8, {width: '20%', ease:Power2.easeInOut}, '-=0.8')
+        .to(redvus_menu, 0.8, {x: '-50%', left: 0, rotation: '-405', ease:Power1.easeInOut}, '-=0.8')
+        .to(redvus_menu_v, 0.6, {y: '-100', autoAlpha: 0, ease:Power1.easeOut}, '-=0.6')
+        .to(redvus_menu_up, 0.6, {autoAlpha: 1, ease:Power1.easeOut}, '-=0.6')
+        .to(redvus_menu_down, 0.6, {autoAlpha: 1, ease:Power1.easeOut}, '-=0.6')
+        .to(redvus_menu_cross, 0.6, {scale: 1, autoAlpha: 1, ease:Power1.easeOut}, '-=0.6')
+        ;
 
     redvus_menu.on('click', function () {
         redvus_menu_open.reversed() ? redvus_menu_open.restart() : redvus_menu_open.reverse();
